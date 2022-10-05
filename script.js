ede396541f83b1bdc504218126fe8de3823b5c36
Hasan Noor <eyehrtsoupy@hasans-imac.mynetworksettings.com>
2022-10-05 14:45:06 -0400

    index.html

---
+++ script.js
@@ -0,0 +1,10 @@
+// when I click START, I'm directed to .questions.
+// when I click START, .time-and-score begins to count-down from 75.
+
+// when I click an item in .incorrect-choice, it turns red.
+// when I click an item in .correct-choice, it turns green.
+
+// when I click an item in .incorrect-choice, "Incorrect!" is displayed below .questions and 10 seconds are subtracted from .time-and-score.
+// when I click an item in .correct-choice, "Correct!" is displayed below .questions.
+
+// when I answer a question, I'm directed to the next question.
